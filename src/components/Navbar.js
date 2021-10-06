c36d5467c5e66fcc16b0d2e8bd82c561498dd031
Antoine.HTML <76158618+Antoinehtml@users.noreply.github.com>
2021-10-06 11:41:06 +0200

    card section done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
 			<nav className="navbar">
 				<div className="navbar-container">
 					<Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
-						system.<i className="fas fa-industry"></i>
+						system. <i className="fas fa-industry"></i>
 					</Link>
 					<div className="menu-icon" onClick={handleClick}>
 						<i className={click ? 'fas fa-times' : 'fas fa-bars'} />
